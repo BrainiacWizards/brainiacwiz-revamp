bf2112fa0901f067262b674c87eab2706c8d3cb0
mmpotulo28 <mpotulom28@gmail.com>
2025-10-05 10:54:58 +0200

    feat: add Prisma optimization extensions for enhanced performance

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { withOptimize } from "@prisma/extension-optimize";
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+const prisma = new PrismaClient()
+	.$extends(withAccelerate())
+	.$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY! }));
 export default prisma;
